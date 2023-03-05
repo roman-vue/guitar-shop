0e9cad00617f103e187cde75807e814108e4747c
roman-laptop <roman.calderin.25@gmail.com>
2023-03-05 16:31:23 -0500

    :hammer:

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
-    TypeOrmModule.forRoot({}),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_DATABASE,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities:true,
+      synchronize: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
